19d984d650273d766242d5586cbe5dfd191f50a5
Mario Nitchev <marionitchev@gmail.com>
2024-03-14 16:55:33 +0200

    revert arn change

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ import (
 	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
 	// to ensure that exec-entrypoint and run can make use of them.
 
+	"go.uber.org/zap/zapcore"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -32,8 +33,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	"github.com/aws/aws-sdk-go/aws/arn"
 
 	"github.com/giantswarm/aws-crossplane-cluster-config-operator/controllers"
 	// +kubebuilder:scaffold:imports
@@ -68,21 +67,10 @@ func main() {
 			"Enabling this will ensure there is only one active controller manager.")
 	opts := zap.Options{
 		Development: true,
+		TimeEncoder: zapcore.RFC3339TimeEncoder,
 	}
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
-
-	_, err := arn.Parse(assumeRoleARN)
-	if err != nil {
-		setupLog.Error(err, "unable to parse assume role ARN")
-		os.Exit(1)
-	}
-
-	_, err = arn.Parse(providerRoleARN)
-	if err != nil {
-		setupLog.Error(err, "unable to parse assume role ARN")
-		os.Exit(1)
-	}
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
